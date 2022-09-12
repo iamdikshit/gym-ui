df0b892d7bbfbe507112ca264e7e116491ffec9f
iamdikshit <dikshit.b.122@gmail.com>
2022-09-12 17:30:57 +0530

    Footer completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
         hero: " linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)), url('/img/hero.jpg')",
         hero2:
           "linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)),url('/img/hero2.png')",
+        footerbg: "url('/img/footer-background.png')",
+        footerbg2:
+          "linear-gradient(rgba(0, 0, 0, 0.8),rgba(0, 0, 0, 0.8)),url('/img/footer2.jpg')",
       },
       colors: {
         "primary-red": "#FF0000",
